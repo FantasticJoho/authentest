7ed66a0873cfc5d835b5c9c25f171145d695ac18
Joho <jonathan.honore@gmail.com>
2026-05-29 16:14:57 +0200

    feat: add user seeding logic to initialize database with default users

--- AuthTest.Api/Program.cs
+++ AuthTest.Api/Program.cs
@@ -41,6 +41,21 @@ builder.Services.AddSingleton<ChallengeStore>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var seedUsers = new[] {
+        new AuthTest.Api.Models.User { Username = "jonathan", PasswordHash = BCrypt.Net.BCrypt.HashPassword("Pas$word2", 12), MustChangePassword = false },
+        new AuthTest.Api.Models.User { Username = "virginie",  PasswordHash = BCrypt.Net.BCrypt.HashPassword("Pas$word2", 12), MustChangePassword = false }
+    };
+    foreach (var u in seedUsers)
+    {
+        if (!db.Users.Any(x => x.Username.ToLower() == u.Username.ToLower()))
+            db.Users.Add(u);
+    }
+    db.SaveChanges();
+}
+
 app.UseCors("WebFormsPolicy");
 app.UseMiddleware<ApiKeyMiddleware>();
 app.MapControllers();
